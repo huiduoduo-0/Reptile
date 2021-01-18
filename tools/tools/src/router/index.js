f1e965213e7fc215b334743f8a8a072bd7668062
yanglele <2715752949@qq.com>
2021-01-18 09:16:20 +0800

    yll

--- tools/tools/src/router/index.js
+++ tools/tools/src/router/index.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import login from '../views/login.vue'
-
+import register from '../views/register.vue'
+import ForgetThePassword from '../views/ForgetThePassword.vue'
+import Password from '../views/Password.vue'
 Vue.use(Router)
 
 export default new Router({
@@ -12,6 +14,21 @@ export default new Router({
       name: 'login',
       component: () => import('../views/login.vue'),
      
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('../views/register.vue'),
+    },
+    {
+      path: '/ForgetThePassword',
+      name: 'ForgetThePassword',
+      component: () => import('../views/ForgetThePassword.vue'),
+    },
+    {
+      path: '/Password',
+      name: 'Password',
+      component: () => import('../views/Password.vue'),
     }
   ]
 })
